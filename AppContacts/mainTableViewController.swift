6be22d2b7548216a54e9f2e59e4da65703a0741d
Kapa <smallfishkapa@mail.ru>
2022-01-27 14:22:14 +0300

    workingwithcoredata

--- AppContacts/mainTableViewController.swift
+++ AppContacts/mainTableViewController.swift
@@ -11,25 +11,23 @@ import CoreData
 class MyContactsListTableViewController: UITableViewController {
     
     var Contacts: [Contact] = []
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
               let context = appDelegate.persistentContainer.viewContext
               
               let fetchRequest: NSFetchRequest<Contact> = Contact.fetchRequest()
-              
+              let sortDescritor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescritor]
               do {
                   Contacts = try context.fetch(fetchRequest)
               } catch let error as NSError {
                   print(error.localizedDescription)
               }
-
     }
     
-   
-    
-    func createContact(withTitle name: String) {
+    func createContact(withTitle name: String, number: String, imagePhoto: Data) {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
@@ -40,26 +38,42 @@ class MyContactsListTableViewController: UITableViewController {
             try context.save()
             Contacts.append(contactObject)
             contactObject.name = name
-            
+            contactObject.phoneNumber = number
+            contactObject.imagePhoto = imagePhoto
         } catch let error as NSError {
             print(error.localizedDescription)
         }
-        
     }
     
     @IBAction func goBack(segue: UIStoryboardSegue) {
         guard segue.identifier == "savecontact", let svc = segue.source as? AddContactTableViewController else {return}
-        var name = svc.nameTF.text
-        createContact(withTitle: name!)
+        let name = svc.nameTF.text
+        let number = svc.numberTF.text
+        let miniPhoto = svc.imagePicked.image
+        let pngPhoto = miniPhoto?.pngData()
+        
+        createContact(withTitle: name!, number: number!, imagePhoto: pngPhoto!)
         self.tableView.reloadData()
         print("666")
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-       
+        
+//        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+//        let fetchRequest: NSFetchRequest<Contact> = Contact.fetchRequest()
+//        if let objects = try? context.fetch(fetchRequest) {
+//            for task in objects {9
+//                context.delete(task)
+//            }
+//        }
+//
+//        do {
+//            try context.save()
+//
+//        } catch let error as NSError {
+//            print(error.localizedDescription)
+//        }
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -71,59 +85,17 @@ class MyContactsListTableViewController: UITableViewController {
         return Contacts.count
     }
 
-  
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "formain", for: indexPath) as! MainTableViewCell
 
         cell.nameLabel.text = Contacts[indexPath.row].name
-
+        cell.phoneLabel.text = Contacts[indexPath.row].phoneNumber
+        cell.miniPhoto.image = Contacts[indexPath.row].imagePhoto as! UIImage?
+        
+//        cell.miniPhoto.contentMode = .scaleAspectFill
+//        cell.miniPhoto.clipsToBounds = true
+//
+        
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
